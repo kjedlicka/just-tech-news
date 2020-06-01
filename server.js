4c51b7233f8b3ba0a295313f17085728914ed4da
Keith Jedlicka <kjedlicka@gmail.com>
2020-06-01 15:36:35 -0400

    added login

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ const hbs = exphbs.create({});
 
 const routes = require('./controllers');
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
